c5a3fd4757c103b9b4d9e132ae778e0d1eef5e21
Cody Merritt <cody.merritt@icloud.com>
2020-08-05 15:49:19 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+      return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
+
+collection = (1..100).to_a
+hello = my_find(collection) {|n| n % 3 == 0 && n % 5 == 0}
+puts hello
